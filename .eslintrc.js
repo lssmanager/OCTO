1d2ff508c779f8ac6953adc84a1039d9f9b20799
lssmanager <manager@learnsocialstudies.com>
2026-05-18 20:08:52 -0500

    fix: reorder exports (types first) + remove legacy .eslintrc.js to fix next lint

--- .eslintrc.js
+++
@@ -1,4 +0,0 @@
-// This file is intentionally a no-op.
-// ESLint 9 flat config is in eslint.config.js.
-// When eslint.config.js is present, ESLint 9 ignores this file automatically.
-module.exports = {};
